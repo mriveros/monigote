a0dc19a22796f3580dc77d0c9154978c3ae28586
Marcos R <mriveros.py@gmail.com>
2022-05-24 22:23:00 -0400

    new file:   app/models/cuota.rb 	new file:   app/models/cuota_detalle.rb

---
+++ app/models/cuota.rb
@@ -0,0 +1,12 @@
+class Cuota < ActiveRecord::Base
+
+  self.table_name="cuotas"
+  self.primary_key="id"
+  
+  attr_accessible :id, :fecha_generacion, :mes_periodo_id, :periodo_escolar_id, :nivel_id, :sala_id, :sucursal_id, :total_cuotas,:created_at, :updated_at
+  
+  scope :orden_01, -> { order("id")}
+  scope :periodo_, -> { order("mes_periodo, anho_periodo DESC")}
+
+  
+end
