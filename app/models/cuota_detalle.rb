820a6a6b668083d2fd3229316f4a13c2ff237473
Marcos R <mriveros.py@gmail.com>
2022-05-24 22:23:00 -0400

    new file:   app/models/cuota.rb 	new file:   app/models/cuota_detalle.rb

---
+++ app/models/cuota_detalle.rb
@@ -0,0 +1,10 @@
+class CuotaDetalle < ActiveRecord::Base
+
+  self.table_name="cuotas_detalles"
+  self.primary_key="id"
+  
+  attr_accessible :id, :cuota_id, :alumno_id, :monto_cuota, :pago_parcial, :pago_pendiente, :estado_pago_cuota_detalle_id, :fecha_pago, :created_at, :updated_at
+  
+  scope :orden_01, -> { order("id")}
+  
+end
