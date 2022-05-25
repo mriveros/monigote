111dd6cd92cbbc338205aa1ca69f176197d8b2cf
Marcos R <mriveros.py@gmail.com>
2022-05-24 22:27:09 -0400

    new file:   app/controllers/cuotas_controller.rb 	modified:   app/models/cuota.rb 	new file:   app/models/v_cuota.rb 	new file:   app/models/v_cuota_detalle.rb

---
+++ app/models/v_cuota_detalle.rb
@@ -0,0 +1,10 @@
+class VCuotaDetalle < ActiveRecord::Base
+
+  self.table_name="v_cuotas_detalles"
+  self.primary_key="id"
+  
+  attr_accessible :id, :cuota_id, :alumno_id, :nombre_alumno, :monto_cuota, :pago_parcial, :pago_pendiente, :estado_pago_cuota_detalle_id, :estado_oago_cuota_detalle, :fecha_pago, :created_at, :updated_at
+  
+  scope :orden_01, -> { order("id")}
+  
+end
