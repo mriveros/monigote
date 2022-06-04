7c4dcd6163f584329fdde7858e563cf38a8d7e32
Marcos R <mriveros.py@gmail.com>
2022-06-04 14:44:46 -0400

    modified:   app/controllers/cuotas_controller.rb 	modified:   app/models/v_cuota.rb 	modified:   app/views/cuotas/_lista.html.erb 	modified:   app/views/cuotas/guardar.js.erb 	modified:   app/views/cuotas/index.html.erb 	modified:   app/views/cuotas/lista.js.erb

--- app/models/v_cuota.rb
+++ app/models/v_cuota.rb
@@ -1,11 +1,11 @@
 class VCuota < ActiveRecord::Base
 
   self.table_name="v_cuotas"
-  self.primary_key="id"
+  self.primary_key="cuota_id"
   
-  attr_accessible :id, :fecha_generacion, :mes_periodo_id, :mes_periodo, :periodo_escolar_id, :anho_periodo, :nivel_id, :nivel, :sala_id, :sala, :sucursal_id, :sucursal, :total_cuotas, :created_at, :updated_at
+  attr_accessible :cuota_id, :fecha_generacion, :mes_periodo_id, :mes_periodo, :periodo_escolar_id, :anho_periodo, :nivel_id, :nivel, :sala_id, :sala, :sucursal_id, :sucursal, :total_cuotas, :created_at, :updated_at
   
-  scope :orden_01, -> { order("id")}
+  scope :orden_01, -> { order("cuota_id")}
   scope :periodo_, -> { order("mes_periodo, anho_periodo DESC")}
 
   
