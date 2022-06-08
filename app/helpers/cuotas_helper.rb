b8b793d79f422512ca68eb4a29ab7e8b1b6f5083
Marcos R <mriveros.py@gmail.com>
2022-06-07 22:08:01 -0400

    modified:   app/controllers/cuotas_controller.rb 	modified:   app/helpers/cuotas_helper.rb 	modified:   app/views/cuotas/_cuota_detalle.html.erb 	modified:   app/views/cuotas/_link_to_pago_cuota_detalle.html.erb 	modified:   app/views/cuotas/_pago_cuota_detalle.html.erb

--- app/helpers/cuotas_helper.rb
+++ app/helpers/cuotas_helper.rb
@@ -6,9 +6,9 @@ module CuotasHelper
       
   	end
 
-  	def link_to_pago_cuota_detalle(cuota_id)
+  	def link_to_pago_cuota_detalle(cuota_detalle_id)
 
-      render partial: 'link_to_pago_cuota_detalle', locals: { cuota_id: cuota_id }
+      render partial: 'link_to_pago_cuota_detalle', locals: { cuota_detalle_id: cuota_detalle_id }
       
   	end
   	
