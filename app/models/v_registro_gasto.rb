dd25976724f4384ba874ce3e542f94e5c13ea6f1
Marcos Riveros <mriveros.py@gmail.com>
2022-06-23 14:03:11 -0400

    adding module registro de gastos

---
+++ app/models/v_registro_gasto.rb
@@ -0,0 +1,14 @@
+class VRegistroGasto < ActiveRecord::Base
+  
+  	self.table_name = "v_registros_gastos"
+	
+	self.primary_key = "registro_gasto_id"
+
+  	attr_accessible :registro_gasto_id, :gasto_id, :gasto, :fecha, :monto, :observacion, :created_at, :updated_at
+ 
+  	scope :orden_01, -> { order("registro_gasto_id")}
+  	scope :orden_fecha_desc, -> { order("fecha DESC")}
+
+
+  
+end
